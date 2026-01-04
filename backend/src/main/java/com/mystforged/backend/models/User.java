30c89ed05b92d80c2c02d539aa6dbec34a4ce19c
GusHPaiva <gassuncaopaiva@gmail.com>
2026-01-04 01:22:18 +0100

    feat: Creation of a User model

---
+++ backend/src/main/java/com/mystforged/backend/models/User.java
@@ -0,0 +1,40 @@
+package com.mystforged.backend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String name;
+
+    @NotBlank
+    @Column(unique = true)
+    private String username;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
