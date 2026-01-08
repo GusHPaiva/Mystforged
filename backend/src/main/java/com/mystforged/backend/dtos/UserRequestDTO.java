fbc3bd5f113776a3be9bb0a2b229224814d8ac04
gusta <@DiretoriaProjetos_include>
2026-01-08 11:16:15 -0300

    refactor: move dto package to correct layer

--- backend/src/main/java/com/mystforged/backend/dtos/UserRequestDTO.java
+++ backend/src/main/java/com/mystforged/backend/dtos/UserRequestDTO.java
@@ -1,4 +1,4 @@
-package dtos;
+package com.mystforged.backend.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
--- backend/src/main/java/com/mystforged/backend/dtos/UserResponseDTO.java
+++ backend/src/main/java/com/mystforged/backend/dtos/UserResponseDTO.java
@@ -1,4 +1,4 @@
-package dtos;
+package com.mystforged.backend.dtos;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
