7d404cad08fe8d4e49cd8db28500fb544a6411b4
gusta <@DiretoriaProjetos_include>
2026-01-08 11:07:45 -0300

    feat: create user request and user response DTOs

---
+++ backend/src/main/java/dtos/UserRequestDTO.java
@@ -0,0 +1,30 @@
+package dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+public record UserRequestDTO(@Size(max = 50)String name,
+
+                             @NotBlank
+                             @Size(min = 3, max = 50)
+                             @Pattern(
+                                     regexp = "^[a-zA-Z0-9._]+$",
+                                     message = "Username cannot contain spaces or special characters"
+                             )
+                             String username,
+
+                             @NotBlank
+                             @Email
+                             String email,
+
+                             @NotBlank
+                             @Size(min = 8, max = 128)
+                             @Pattern(
+                                     regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).*$",
+                                     message = "The password must be longer than 8 characters, contain a number, a lower case and uppercase letter and an special character"
+                             )
+                             String password ) {
+
+}
