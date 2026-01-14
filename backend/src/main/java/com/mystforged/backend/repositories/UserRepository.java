40219f5e1f24e5ff819ab8e03829d77dcbc2d91a
GusHPaiva <gassuncaopaiva@gmail.com>
2026-01-13 23:31:11 -0300

    feat: add necessary imports to UserRepository

--- backend/src/main/java/com/mystforged/backend/repositories/UserRepository.java
+++ backend/src/main/java/com/mystforged/backend/repositories/UserRepository.java
@@ -1,8 +1,11 @@
 package com.mystforged.backend.repositories;
 
 import com.mystforged.backend.models.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository <User, UUID>{
