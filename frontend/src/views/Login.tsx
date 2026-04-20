101980bf5b448e81000335eb58cad6c3202bd70f
GusHPaiva <gassuncaopaiva@gmail.com>
2026-04-20 20:11:30 -0300

    feat: add user feedback to login page

---
+++ frontend/src/views/Login.tsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import '../index.css'
+import { api } from '../config/api'
+import { Link, useNavigate } from 'react-router-dom'
+
+function Login() {
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMsg, setErrorMsg] = useState('')
+    const [successMsg, setSuccessMsg] = useState('')
+
+    async function LoginRequest(email: string, password: string) {
+        try {
+            const response = await api.post('/auth/login', { email, password })
+            
+            if (response.data.token) {
+                localStorage.setItem('token', response.data.token)
+                setSuccessMsg("Login successful!Redirecting...")
+                
+                setTimeout(() => {
+                    navigate('/') 
+                }, 1500)
+            }
+        } catch (error: any) {
+            console.error('Error logging in:', error)
+            if (error.response && (error.response.status === 401 || error.response.status === 403)) {
+                setErrorMsg("Invalid email or password.")
+            } else {
+                setErrorMsg("Error connecting to the server.")
+            }
+        }
+    }
+
+    function handleLogin(e: React.FormEvent) {
+        e.preventDefault()
+        setErrorMsg('')
+        setSuccessMsg('')
+        LoginRequest(email, password)
+    }
+
+  return (
+    <div className="bg-myst items-center justify-center flex min-h-dvh w-full overflow-x-hidden bg-cover bg-center">
+      <div className="bg-card/40 text-primary-txt w-[90%] max-w-sm justify-center items-center flex flex-col gap-6 p-10 rounded-lg shadow-lg backdrop-blur-[2px] border border-white/20">
+      <div className="w-full flex justify-center select-none">
+         <img src="/src/assets/MystLogo.png" alt="Logo MystForged" className="w-full max-w-50 sm:max-w-xs mx-auto h-auto object-contain select-none pointer-events-none" />
+      </div>
+        <div className="flex flex-col gap-4 w-full">
+            <form onSubmit={handleLogin} className="flex flex-col gap-4">
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Email" className="rounded-md bg-card text-primary-txt border border-border focus:outline-none focus:ring-2 focus:ring-action p-2" />
+            <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Password" className="rounded-md bg-card text-primary-txt border border-border focus:outline-none focus:ring-2 focus:ring-action p-2" />
+            <button className="bg-action text-primary-txt hover:bg-hover rounded-md p-3">Login</button>
+            </form>
+            
+            {successMsg && <p className="text-green-500 font-bold text-sm text-center text-wrap">{successMsg}</p>}
+            {errorMsg && <p className="text-red-500 font-bold text-sm text-center text-wrap">{errorMsg}</p>}
+        </div>
+        <div className="flex flex-col items-center gap-2">
+            <p className='text-sm text-primary-txt/80'>Don't have an account? <Link to="/register" className='text-secundary-txt hover:text-hover'>Sign up</Link></p>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login;
