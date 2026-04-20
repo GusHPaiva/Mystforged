571e6cfd3810d3a274323324877586edab46d641
GusHPaiva <gassuncaopaiva@gmail.com>
2026-04-20 20:11:06 -0300

    chore: add new routes

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router-dom'
-import Login from './Login' 
-
+import Login from './views/Login' 
+import Register from './views/Register'
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   )
 }
